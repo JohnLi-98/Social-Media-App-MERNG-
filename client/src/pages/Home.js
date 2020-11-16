1ee0cf9ac0e40cafe0c4ba3eeb99b182dec442e1
John Li <hud_johnli@live.co.uk>
2020-11-16 22:22:11 +0000

    authenticated users are now able to add posts onto the timeline

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useQuery } from '@apollo/client';
-import { Grid, Transition } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 
+import { AuthContext } from '../context/auth';
 import PostCard from '../components/PostCard';
+import PostForm from '../components/PostForm';
 import { FETCH_POSTS_QUERY } from '../util/graphql';
 
 function Home() {
+    const { user } = useContext(AuthContext);
     const {
         loading, data: { getPosts: posts } = {}
     } = useQuery(FETCH_POSTS_QUERY);
@@ -17,6 +20,11 @@ function Home() {
             </Grid.Row>
 
             <Grid.Row>
+                { user && (
+                    <Grid.Column>
+                        <PostForm />
+                    </Grid.Column>
+                )}
                 {loading ? (
                     <h1>Loading posts...</h1>
                 ) : (
