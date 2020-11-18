b195028e5ea82b57a7155424e426a1b420c83223
John Li <hud_johnli@live.co.uk>
2020-11-18 22:20:46 +0000

    made changes to code for deployment

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const { MONGODB } = require('./config.js');
 // Instantiate and pass to context, so it can be used in the resolvers
 const pubsub = new PubSub();
 
+const PORT = process.env.port || 5000;
+
 // set up Apollo server, context takes a callback, which gets anything that was passed before the 
 // apollo server. You get the request and response from express. Destructure the request and forward 
 // it to the context, where you can access the request body.
@@ -29,9 +31,12 @@ mongoose
     .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to MongoDB');
-        return server.listen({ port: 5000 });
+        return server.listen({ port: PORT });
     })
     .then(res => {
         console.log(`Server running at ${res.url}`)
-    });
+    })
+    .catch(err => {
+        console.log(err);
+    })
 
